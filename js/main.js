002fa7d2835c98a60f4bce7d8e74f4e350f25276
Igormandello <igormandello@gmail.com>
2017-10-02 21:48:45 -0300

    Makes the page responsive and changes some colors
    - The page fits on mobile and desktop
    - The page fits on portrait or landscape mode
    - Colors are now specified at the top of the document, inside an array,
    which makes it easy to change

--- js/main.js
+++ js/main.js
@@ -1,50 +1,79 @@
+var colors = 
+{ 
+  background: [ 0x090A31, 0x030425, 0x262761, 0x0D1075 ][0],
+  circle: 0x3745ab,
+  line: 0x2248d0
+}
+
+var app;
+var g = new PIXI.Graphics();
+
 var points = [];
 var pointsNumber = 120;
 var maxDist = 120;
 
 $(document).ready(function(){
     initPixi();
+    window.onresize = () => resizePixi()
 });
 
 function initPixi()
 {
-    var app = new PIXI.Application(document.body.clientWidth, document.body.clientHeight, { backgroundColor: 0x30316b, antialias: true });
+    app = new PIXI.Application(document.body.clientWidth, document.body.clientHeight, { backgroundColor: colors.background, antialias: true });
     $("#containerBackground")[0].appendChild(app.view);
     
     for (let n = 0; n < pointsNumber; n++)
-        points.push(new Point(Math.random() * document.body.clientWidth, Math.random() * document.body.clientHeight, Math.random() * 3 + 5, Math.random() * 2 * Math.PI, Math.random() * 2 + 1));
+        points.push(new Point(Math.random() * document.body.clientWidth, Math.random() * document.body.clientHeight, Math.random() * 3 + 5, Math.random() * 2 * Math.PI, Math.random() + 1));
         
-    var g = new PIXI.Graphics();
     app.stage.addChild(g);
-    
-    app.ticker.add(function() {
-        g.clear();
-        g.beginFill(0x3745ab, 1);
-        
-        for (var n = 0; n < points.length; n++)
-        {
-            points[n].update(document.body.clientWidth, document.body.clientHeight);
-            
-            for(var i = 0; i < points.length; i++)
-            {
-                var dist = points[n].distance(points[i]);
-                if (dist < maxDist)
-                {
-                    g.lineStyle(2, 0x2248d0, 1 - dist / maxDist);
-                    g.moveTo(points[n].x, points[n].y);
-                    g.lineTo(points[i].x, points[i].y);
-                }
-            }    
-        }
-        
-        for (var n = 0; n < points.length; n++)
-        {
-            g.lineStyle(0, 0x000, 1);
-            g.drawCircle(points[n].x, points[n].y, points[n].radius);
-        }
+    app.ticker.add(drawPoints);
+}
 
-        g.endFill();
-    });
+function drawPoints(delta)
+{
+    g.clear();
+    g.beginFill(colors.circle, 1);
+
+    for (var n = 0; n < points.length; n++)
+    {
+        points[n].update(document.body.clientWidth, document.body.clientHeight, delta);
+
+        for(var i = 0; i < points.length; i++)
+        {
+            var dist = points[n].distance(points[i]);
+            if (dist < maxDist)
+            {
+                g.lineStyle(2, colors.line, 1 - dist / maxDist);
+                g.moveTo(points[n].x, points[n].y);
+                g.lineTo(points[i].x, points[i].y);
+            }
+        }    
+    }
+
+    for (var n = 0; n < points.length; n++)
+    {
+        g.lineStyle(0, 0x000, 1);
+        g.drawCircle(points[n].x, points[n].y, points[n].radius);
+    }
+
+    g.endFill();
+}
+
+function resizePixi()
+{
+  points = [];
+  
+  var container = $("#containerBackground")[0];
+  container.removeChild(app.view);
+  
+  app.ticker.remove(drawPoints);
+  initPixi();
+  /*
+  var container = $("#containerBackground")[0];
+  
+  container.children[0].width = document.body.clientWidth;
+  container.children[0].height = document.body.clientHeight;
+  */
 }
 
 function Point(x, y, radius, angle, speed)
@@ -55,7 +84,7 @@ function Point(x, y, radius, angle, speed)
     this.moduleY = Math.sin(angle) * speed;
     this.radius = radius;
     
-    this.update = (maxX, maxY) =>
+    this.update = (maxX, maxY, delta) =>
     {
         if (this.x + this.radius * 2 < 0)
             this.x = maxX;
@@ -67,8 +96,8 @@ function Point(x, y, radius, angle, speed)
         else if (this.y > maxY)
             this.y = 0;
         
-        this.x += this.moduleX;
-        this.y += this.moduleY;
+        this.x += this.moduleX * delta;
+        this.y += this.moduleY * delta;
     }
     
     this.distance = (other) =>
