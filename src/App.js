d53e961b87dfc03770504150aff2288561341b27
Igormandello <igormandello@gmail.com>
2018-10-01 13:50:14 -0300

    Finishes the page structuring
    - Adds a test image to check if the components images are working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import Intro from './components/Intro';
+import AboutMeSection from './components/AboutMeSection';
+import Monitor from './components/Monitor';
+import Input from './components/Input';
+import PopMedia from './components/PopMedia';
 import './css/Normalize.css';
 
 class App extends Component {
@@ -20,6 +24,81 @@ class App extends Component {
           <h1>Igor Mandello</h1>
           <a></a>
         </Intro>
+        <section>
+          <h2>Sobre Mim</h2>
+          <AboutMeSection image="image.png">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas 
+              delectus earum quaerat? Doloribus, obcaecati quo illo mollitia 
+              culpa totam voluptatum voluptate maiores voluptatem nemo nihil 
+              quod neque nostrum expedita nam?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas 
+              delectus earum quaerat? Doloribus, obcaecati quo illo mollitia 
+              culpa totam voluptatum voluptate maiores voluptatem nemo nihil 
+              quod neque nostrum expedita nam?
+            </p>
+          </AboutMeSection>
+          <AboutMeSection image="image.png">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas 
+              delectus earum quaerat? Doloribus, obcaecati quo illo mollitia 
+              culpa totam voluptatum voluptate maiores voluptatem nemo nihil 
+              quod neque nostrum expedita nam?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas 
+              delectus earum quaerat? Doloribus, obcaecati quo illo mollitia 
+              culpa totam voluptatum voluptate maiores voluptatem nemo nihil 
+              quod neque nostrum expedita nam?
+            </p>
+          </AboutMeSection>
+        </section>
+        <section>
+          <h2>Projetos</h2>
+          <Monitor />
+          <div>
+            <h3>Lorem Ipsum</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas 
+              delectus earum quaerat? Doloribus, obcaecati quo illo mollitia 
+              culpa totam voluptatum voluptate maiores voluptatem nemo nihil 
+              quod neque nostrum expedita nam?
+            </p>
+            <a>
+              <i></i>
+              Veja mais no GitHub
+            </a>
+          </div>
+
+          <div>
+          </div>
+        </section>
+        <section>
+          <h2>Contato</h2>
+          <div>
+            <div>
+              <Input />
+              <Input />
+            </div>
+            <Input />
+            <Input />
+
+            <a>
+              Enviar
+            </a>
+          </div>
+          <div>
+            <PopMedia />
+            <PopMedia />
+            <PopMedia />
+            <PopMedia />
+          </div>
+        </section>
+        <footer>
+          Igor Mandello © 2018
+        </footer>
       </div>
     );
   }
