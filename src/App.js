a2af207ad6c04247d1aaf7aacdc5de92f6290cc4
Igormandello <igormandello@gmail.com>
2018-10-01 11:54:52 -0300

    Structures the header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Intro points={100} maxDist={120}/>
+        <header>
+          <nav>
+            <a>Sobre mim</a>
+            <a>Projetos</a>
+            <a>Contato</a>
+          </nav>
+          <div>
+            <img />
+          </div>
+        </header>
+        <Intro points={100} maxDist={120}>
+          <h1>Igor Mandello</h1>
+          <a></a>
+        </Intro>
       </div>
     );
   }
