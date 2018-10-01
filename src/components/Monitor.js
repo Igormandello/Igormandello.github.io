6240a4dade32c3db0440d31cecd22aec5d288b5e
Igormandello <igormandello@gmail.com>
2018-10-01 13:46:56 -0300

    Creates Monitor component

---
+++ src/components/Monitor.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Monitor(props) {
+  return (
+    <div>
+      <div>
+        <div>
+          <img />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Monitor;
