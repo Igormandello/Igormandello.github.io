49915ce00bc91dbc72c23cfc8969eeccafcf0a2c
Igormandello <igormandello@gmail.com>
2018-10-03 07:57:29 -0300

    Updates the Input to accept more types

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 function Input(props) {
+  let input;
+  if (props.type === 'text' || !props.type)
+    input = <input type="text" id={props.id}/>;
+  else if (props.type !== 'textarea')
+    input = <input type={props.type} id={props.id}/>
+  else
+    input = <textarea id={props.id} className="materialize-textarea" />
+
   return (
-    <div>
-      <input type="text" id={props.id}/>
-      <label for={props.id}></label>
+    <div className={'input-field' + (props.className ? ' ' + props.className : '')}>
+      {input}
+      <label for={props.id}>{props.label}</label>
     </div>
   );
 }
