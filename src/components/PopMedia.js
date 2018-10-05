a2b0494ac51d37542cbdac6f313872b636c1bb71
Igormandello <igormandello@gmail.com>
2018-10-05 10:50:44 -0300

    Creates the PopMedia component
    - The PopMedia expands whenever the user clicks on it

--- src/components/PopMedia.js
+++ src/components/PopMedia.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../css/PopMedia.css';
 
-function PopMedia(props) {
-  return (
-    <div>
-      <img />
-      <div>
-        aaa
+class PopMedia extends Component {
+  openMedia(event) {
+    let target = event.target
+    if (event.target.tagName == 'IMG')
+      target = target.parentNode;
+
+    target.parentNode.classList.toggle('active');
+  }
+
+  render() {
+    return (
+      <div className={'popMedia col s6' + (this.props.right ? ' right' : '')}>
+        <div onClick={this.openMedia}>
+          <img src={require('./assets/' + this.props.media.toLowerCase() + '.svg')}/>
+        </div>
+        <div>
+          <span className="title">{this.props.media}</span>
+          <span>{this.props.link}</span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default PopMedia;
